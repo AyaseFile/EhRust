753d2703ade4b55adecefbac0c549f943d173495
Ayase <166034664+AyaseFile@users.noreply.github.com>
2025-03-17 21:04:54 +0800

    feat: nix flakes, direnv

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, fenix, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs, system, ... }:
+        with pkgs;
+        let
+          rust_toolchain = fenix.packages.${system}.stable.withComponents [
+            "cargo"
+            "rustc"
+            "rust-src"
+            "clippy"
+            "rustfmt"
+          ];
+        in
+        {
+          default = mkShell {
+            buildInputs = [
+              rust_toolchain
+              openssl
+              pkg-config
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
+            RUST_SRC_PATH = "${rust_toolchain}/lib/rustlib/src/rust/library";
+          };
+        }
+      );
+    };
+}
